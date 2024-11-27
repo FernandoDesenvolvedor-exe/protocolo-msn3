543080a973f33f77665c28c140bbde8f06a6524e
Luis Fernando Pereira <git@gestorkm.com.br>
2024-11-27 18:21:37 -0300

    alterado formato de base de dados simulada

--- client/client.py
+++ client/client.py
@@ -1,5 +1,6 @@
 # O PROTOCOLO SERA COMPOSTO POR CABEÇALHO COM INFORMAÇÕES DE QUEM ENVIOU
 from socket import *
+from datetime import datetime
 #import requests
 import json
 import threading
@@ -12,7 +13,7 @@ import queue
 # Fila para armazenar mensagens recebidas, para exibição na tela
 mensagens_recebidas = queue.Queue() 
 tuplaDestino = ('localhost',12000)
-msn3HeaderParams = ["RS","AUTH","CDS","RDS"]
+msn3HeaderParams = ["AUTH","CDS","RDS"]
 #ip_publico = requests.get('https://api.ipify.org/').text 
 
 
@@ -61,6 +62,16 @@ def translateRequestMethod(method):
         case _:
             return "METODO INVALIDO"  
 
+def enviaRequisicao(msg):
+    msg = msg.encode()
+    socCli = socket( AF_INET, SOCK_STREAM )
+    socCli.connect( tuplaDestino )
+    socCli.send(msg)
+    resp = socCli.recv(1024)
+    socCli.close()
+
+    return parseMsn3Request(resp.decode())
+
 def parseMsn3Request(request):
     if request.find("--H") != -1:
         request = request.split("--H")
@@ -70,7 +81,7 @@ def parseMsn3Request(request):
     resp_h = request[0].strip().split("&")
     resp_b = request[1].strip().split("&")
 
-    requestData = {"header": {}}   
+    requestData = {"header": {}}
 
     for data in resp_h:
         key_data = data.split("=")
@@ -100,27 +111,42 @@ def parseMsn3Request(request):
 
     return requestData
 
-def conversas(userid,limiter):
-    msg = "MET=CDS&SND="+ip_privado+"&RES=Conversas("+str(userid)+","+limiter+")--H "
-    msg = msg.encode()
-    socCli = socket( AF_INET, SOCK_STREAM )
-    socCli.connect( tuplaDestino )
-    socCli.send(msg)
-    resp = socCli.recv(1024)
-    socCli.close()
+def validaResposta(resposta):    
+    if resposta["header"]["STATUS"] == "ERR":
+        raise ConnectionError(resposta["body"])
 
-    return parseMsn3Request(resp.decode())
+    if resposta["header"].get("CNTT") != None and resposta["header"].get("CNTT") == "json":
+        resposta["body"] = json.loads(resposta["body"][0])
+        
+    return resposta
+
+def contatos(user_id):
+    msg = "MET=CDS&SND="+ip_privado+"&RES=Contatos("+str(user_id)+")--H "
+    return validaResposta(enviaRequisicao(msg))
+
+def conversas(user_id,contact_id):
+    msg = "MET=CDS&SND="+ip_privado+"&RES=Conversas("+str(user_id)+","+contact_id+")--H "
+    return validaResposta(enviaRequisicao(msg))
 
 def login(username,password):
     msg = "MET=AUTH&SND="+ip_privado+"&RES=Login("+username+","+password+")--H"
-    msg = msg.encode()
-    socCli = socket( AF_INET, SOCK_STREAM )
-    socCli.connect( tuplaDestino )
-    socCli.send(msg)
-    resp = socCli.recv(1024)
-    socCli.close()
-    
-    return parseMsn3Request(resp.decode())
+    return validaResposta(enviaRequisicao(msg))
+
+def novaMensagem(user_id,contact_id,msg):
+    datahora = datetime.now()
+    datahora = datahora.strftime("%Y:%m:%d %H:%M:%S")
+    msg = {
+        "sender_id": user_id,
+        "reciver_id":contact_id,
+        "message":msg,
+        "datetime":datahora
+    }
+
+    msg = "MET=REG&SND="+ip_privado+"&RES=NovaMensagem()&CNTT=json--H "+json.dumps(msg)
+    return validaResposta(enviaRequisicao(msg))
+
+def alignCenter():
+    return "                                        "
 
 ip_privado = getPrivateIp()
 
@@ -129,16 +155,16 @@ ip_privado = getPrivateIp()
 while True:    
     print("")
     print("")
-    print("-----------------------------")
-    print("App de Mensagens")
-    print("-----------------------------")
+    print(f"{alignCenter()}-----------------------------")
+    print(f"{alignCenter()}App de Mensagens")
+    print(f"{alignCenter()}-----------------------------")
     print("")
     print("")
-    print("1 - Login")
-    print("2 - Sair")
+    print(f"{alignCenter()}1 - Login")
+    print(f"{alignCenter()}2 - Sair")
     print("")
 
-    opcao = int(input("Escolha uma opcao: "))
+    opcao = int(input(f"{alignCenter()}Escolha uma opcao: "))
     match opcao:
         case 1:
             #username = input("Usuario : ")
@@ -147,62 +173,104 @@ while True:
             username = "Fernando"
             password = "123"            
 
-            print("Solicitando authenticacao de usuario")            
+            print(f"{alignCenter()}Solicitando authenticacao de usuario")  
 
-            loginData = login(username,password)
+            try:
+                loginData = login(username,password)
             
-            if loginData["header"]["STATUS"] == "OK":
-                if loginData["body"][0] != "NEG" and loginData["body"][0] != "RNF":
-                    userid = loginData["body"][0]
+                if loginData["body"][0] != "NEG" and loginData["body"][0] != "RNF":                     
+                    print("") 
+                    print(f"{alignCenter()}Usuario Autorizado")      
+                    print("")   
+
+                    user_id = loginData["body"][0]
+                    contatos_salvos = contatos(user_id)
 
                     while True:
-                        print("")
-                        print("")
-                        print("-----------------------------")
-                        print("App de Mensagens - "+username)
-                        print("-----------------------------")
-                        print("")
-                        resp = conversas(userid,"USER_STATUS")      
+                        print(f"""
+                                        -----------------------------
+                                        App de Mensagens - {username}
+                                        -----------------------------
+                                        
+                                        talk - conversar
+                                        new - Novo contato
+                                        quit - Sair"
+                                                """)
+                        opcao = input(f"{alignCenter()}Selecione uma opcao: ")
 
-                        header = resp["header"]
-                        body = resp["body"]
+                        match opcao:
+                            case "new":
+                                print("algo")
+                            case "quit":
+                                break
+                            case "talk"|"Talk"|"TALK":
+                                print("")
+                                print("")                                
+                                contato = []
+                                n = 0                                
+                                for chat_index in contatos_salvos["body"]:
+                                    contato.append(contatos_salvos["body"].get(chat_index))
 
-                        if header["CNTT"] == "json":
-                            print("")
-                            print("talk - conversar")
-                            print("new - Nova contato")
-                            print("quit - Sair")
-                            print("")
-                            opcao = input("Selecione uma opcao: ")
+                                    print(f"{alignCenter()}{contato[int(chat_index)].get("id")}",end=" ")
+                                    print(" - "  + contato[int(chat_index)].get("name"),end=" ")
+                                    print(" "+contato[int(chat_index)].get("status"))
 
-                            match opcao:
-                                case "new":
-                                    print("algo")
-                                case "quit":
-                                    break
-                                case "talk":
-                                    print("")
-                                    print("")
-                                    corpo_conversas = json.loads(body[0])
-                                    contato = []
-                                    n = 0                                
-                                    for chat_index in corpo_conversas:
-                                        contato.append(corpo_conversas.get(chat_index))
+                                print(f"{alignCenter()}quit - Voltar")
+                                opcao = input(f"{alignCenter()}Conversar com: ")
 
-                                        print(contato[int(chat_index)].get("id"),end=" ")
-                                        print(" - "  + contato[int(chat_index)].get("name"),end=" ") 
-                                        print(" "+contato[int(chat_index)].get("status"))
+                                match opcao:
+                                    case "quit":
+                                        break
+                                    case _:
+                                        while True:
+                                            chat = conversas(user_id,opcao)["body"]
+                                            contact_id = opcao
+                                            contact_name = chat.get("messages_sent").get("enderecado")
+                                        
+                                            chat2 = chat.get("messages_received")
+                                            chat = chat.get("messages_sent")
 
-                                    opcao = input("Conversar com: ")
+                                            chat2.pop("enderecado")
+                                            chat.pop("enderecado")
+                                            
+                                            for index in chat2.get("conversa"):
+                                                chat2.get("conversa").get(index).append(contact_name)
 
-                                    resp = conversas(userid,"USER_CHAT",opcao)
+                                            for index in chat.get("conversa"):
+                                                chat.get("conversa").get(index).append(username)
 
-                                case _:
-                                    print("TODO")
+                                            chat.get("conversa").update(chat2.get("conversa"))
+                                            del chat2
+                                            chat = chat["conversa"]
+
+                                            print(len(chat))
+                                            chat = dict(sorted(chat.items()))
+
+                                            print(f"{alignCenter()}---------------------------------")
+                                            print(f"{alignCenter()}Conversa com {contact_name} código({contact_id})")
+                                            print(f"{alignCenter()}---------------------------------")       
+
+                                            for index in chat:
+                                                print(f"""{alignCenter()}{chat[index][2]}: {chat[index][0]} -- {chat[index][1]}""")
+
+                                            print("")
+                                            print(f"{alignCenter()}newm - Nova mensagem")
+                                            print(f"{alignCenter()}quit - voltar")
+                                            option = input(f"{alignCenter()}Opção: ")
+                                            
+                                            match option:
+                                                case "newm":
+                                                    nova_mensagem = input(f"{alignCenter()}Mensagem: ")
+                                                    novaMensagem(user_id,contact_id,nova_mensagem)
+                                                case "quit":
+                                                    break
+
+                            case _:
+                                print("TODO")
                 else:
                     print("Acesso negado")
-            else:
-                print("Erro ao se comunicar com servidor")
+            except ConnectionError:
+                print("Erro ao tentar se cominicar com servidor")
 
         case 2:
             break
--- server/servidor.py
+++ server/servidor.py
@@ -14,7 +14,7 @@ tuplaEscuta = ('',12000)
 socketServidor.bind( tuplaEscuta )
 socketServidor.listen(True)
 
-msn3HeaderMETS = ["RS","AUTH","CDS","RDS"]
+msn3HeaderParams = ["AUTH","CDS","REG"]
 
 def translateRequestMethod(method):
     match method.upper():
@@ -24,42 +24,41 @@ def translateRequestMethod(method):
             return "AUTENTICACAO"
         case "CDS":
             return "CONSULTA DE DADOS DO SERVIDOR"
-        case "RDS":
-            return "REGISTRAR DADOS NO SERVIDOR"
+        case "REG":
+            return "REGISTRAR"
         case _:
             return "METODO INVALIDO"  
 
 def parseMsn3Request(request):
-    request = request.split("--H")
+    if request.find("--H") != -1:
+        request = request.split("--H")
+    elif request.find("--RESP"):
+        request = request.split("--RESP")
+
     resp_h = request[0].strip().split("&")
     resp_b = request[1].strip().split("&")
 
-    requestData = {"header": {}}   
+    requestData = {"header": {}}
 
     for data in resp_h:
         key_data = data.split("=")
         requestData["header"][key_data[0]] = key_data[1]
 
-    if requestData["header"]["MET"] not in msn3HeaderMETS:
-        raise SyntaxError("Metodo nao encontrado")
-
-    if requestData["header"].get("RES") != None: 
-
-        res_param = requestData["header"]["RES"].replace(")","&").replace("(","&").split("&")
-        res_param.pop(2)
-        res =  res_param[0]
-        params = str(res_param[1])
+    try:        
+        if requestData["header"].get("RES") != None: 
+            res_param = requestData["header"]["RES"].replace(")","&").replace("(","&").split("&")
+            res_param.pop(2)
+            res =  res_param[0]
+            params = str(res_param[1])
 
         if params != '':
             params = params.split(",")
 
         requestData["header"]["RES"] = res
         requestData["header"]["RESPR"] = params  
-
-    try:
-        requestData["header"]["CNTT"] = resp_h[3]
-    except IndexError:
-        requestData["header"]["CNTT"] = ""
+    except KeyError:
+        requestData["header"]["RES"] = ""
+        requestData["header"]["RESPR"] = ""    
     
     requestData["body"] = resp_b  
 
@@ -75,43 +74,67 @@ usuarios = {"Fernando": {"senha":"123","id":0, "IP":"0"},
 conversas = {
     "0":{
         "1":{
-            "usuario":"Felipe",
-            "conversa":[
-                ["Fernando","Oi mano","2024-12-25 10:33:03"],
-                ["Felipe","Aew cara","2024-12-25 10:33:40"],
-                ["Fernando","Tudo bem?","2024-12-25 10:33:58"],
-                ["Felipe","Tudo certo aqui mano","2024-12-25 10:34:26"]
-            ]
+            "enderecado":"Felipe",
+            "conversa":{
+                "0":["Oi mano","2024-12-25 10:33:03"],
+                "2":["Tudo bem?","2024-12-25 10:33:58"],
+            }
         },
         "2":{
-            "usuario":"Guilherme",
-            "conversa":[
-                ["Fernando","Oi mano","2024-12-25 11:30:00"],
-                ["Guilherme","Fala","2024-12-25 11:33:40"],
-                ["Fernando","Ja buscou a parada la?","2024-12-25 11:35:30"],
-                ["Guilherme","Sim","2024-12-25 11:36:20"]
-            ]
+            "enderecado":"Guilherme",
+            "conversa":{
+                "0":["Oi mano","2024-12-25 11:30:00"],
+                "2":["Ja buscou a parada la?","2024-12-25 11:35:30"]              
+            }
         },
         "3":{
-            "usuario":"Andressa",
-            "conversa":[
-                ["Fernando","Oi vida","2024-12-25 20:01:03"],
-                ["Andressa","Oii","2024-12-25 20:01:15"],
-                ["Fernando","Como ta a aula?","2024-12-25 20:02:14"],
-                ["Andressa","Entediante","2024-12-25 20:02:23"]
-            ]
+            "enderecado":"Andressa",
+            "conversa":{
+                "0":["Oi vida","2024-12-25 20:01:03"],
+                "2":["Como ta a aula?","2024-12-25 20:02:14"]
+            }
         }
     },
     "1":{
+        "0":{
+            "enderecado":"Fernando",
+            "conversa":{
+                "1":["Aew cara","2024-12-25 10:33:40"],
+                "3":["Tudo certo aqui mano","2024-12-25 10:34:26"]
+            }
+        },
         "2":{
-            "usuario":"Guilherme",
-            "conversa":[
-                ["Felipe","Oi mano","2024-12-25","11:30:00"],
-                ["Guilherme","Fala","2024-12-25","11:33:40"],
-                ["Felipe","Ja buscou a parada la?","2024-12-25","11:35:30"],
-                ["Guilherme","Sim","2024-12-25","11:36:20"]
-            ]
-        }
+            "enderecado":"Guilherme",
+            "conversa":{
+                "0":["Oi mano","2024-12-25","11:30:00"],
+                "2":["Ja buscou a parada la?","2024-12-25","11:35:30"],
+            }
+        }        
+    },
+    "2":{
+        "0":{
+            "enderecado":"Fernando",
+            "conversa":{
+                "1":["Fala","2024-12-25 11:33:40"],
+                "3":["Sim","2024-12-25 11:36:20"]
+            }
+        },
+        "1":{
+            "enderecado":"Felipe",
+            "conversa":{
+                "1":["Fala","2024-12-25","11:33:40"],
+                "3":["Sim","2024-12-25","11:36:20"]
+            }
+        } 
+    },
+    "3":{
+        "0":{
+            "enderecado":"Fernando",
+            "conversa":{
+                "1":["Oii","2024-12-25 20:01:15"],
+                "3":["Entediante","2024-12-25 20:02:23"]
+            }
+        }        
     }
 }
 
@@ -126,7 +149,7 @@ while True:
     print(f"Recebido: {mensagem}")
 
     try:
-        data = parseMsn3Request(mensagem)   
+        data = parseMsn3Request(mensagem)
 
         user_ip = data["header"]["SND"]
         method = data["header"]["MET"]
@@ -149,37 +172,32 @@ while True:
                             response = "SND=SERVER&STATUS=OK--RESP NEG"
                     case _:
                         response = "SND=SERVER&STATUS=ERR--RESP RNF"
-            
             case "CDS":
                 match resource:
-                    case "Conversas":
+                    case "Contatos":
                         id = data["header"]["RESPR"][0]
                         
                         if conversas.get(id) != None:
-                            try:
-                                limiter = data["header"]["RESPR"][1].upper()
+                            try:                             
+                                users = {}
+                                n = 0
 
-                                match limiter:
-                                    case "USER_STATUS":
-                                        users = {}
-                                        n = 0
+                                for talked_to_id in conversas.get(id):
+                                    contactName = conversas.get(id).get(talked_to_id).get("enderecado")
+                                    ip_atual = usuarios.get(contactName).get("IP")
 
-                                        for talked_to_id in conversas.get(id):
-                                            username = conversas.get(id).get(talked_to_id).get("usuario")
-                                            ip_atual = usuarios.get(username).get("IP")
+                                    if ip_atual == "0": 
+                                        state = "OFF" 
+                                    else: 
+                                        state = "ON"                                            
 
-                                            if ip_atual == "0": 
-                                                state = "OFF" 
-                                            else: 
-                                                state = "ON"                                            
+                                    users[n] = {
+                                        "id": talked_to_id,
+                                        "name": contactName,
+                                        "status": state 
+                                    }
 
-                                            users[n] = {
-                                                "id": talked_to_id,
-                                                "name": username,
-                                                "status": state 
-                                            }
-
-                                            n += 1
+                                    n += 1
 
                             except IndexError:
                                 print("deu ruim pai")
@@ -188,15 +206,52 @@ while True:
                             response = "SND=SERVER&STATUS=OK&CNTT=json--RESP "+json.dumps(users)
                         else:
                             response = "SND=SERVER&STATUS=OK--RESP  NUF"
-                                            
+                    case "Conversas":
+                        id_user = data["header"]["RESPR"][0]
+                        id_contact = data["header"]["RESPR"][1]
+                        
+                        if conversas.get(id) != None:
+                            if conversas.get(id).get(id_contact) != None:                                
+                                messages_sent = conversas.get(id).get(id_contact)
+                                messages_received = conversas.get(id_contact).get(id)
+                                chat = {"messages_sent":messages_sent,"messages_received":messages_received}
+                            else: 
+                                chat = {"error":"CNE"} #Contato não encontrado
+                        else:
+                            chat = {"error":"UNE"} #Usuario não encotrado
+                        
+                        response = "SND=SERVER&STATUS=OK&CNTT=json--RESP "+json.dumps(chat,indent=None,separators=(',', ':'))
                     case _:
                         response = "SND=SERVER&STATUS=OK--RESP RNF"
-                    
-            case _:
-                response = "SND=SERVER&STATUS=OK--RESP MNS"
+            case "REG":
+                match resource:
+                    case "NovaMensagem": 
+                        if len(data["body"]) > 0:    
+                            try:
+                                data["body"] = json.loads(data["body"]) 
 
-    except SyntaxError:
-        response = "SND=SERVER&STATUS=ERR--RESP MHP"
+                                id_sender = data["body"][0]
+                                id_receiver = data["body"][1]
+                                message = data["body"][2]
+                                date_time = data["body"][3]
+
+                                new_message = [message, date_time]
+
+                                conversas[id_sender][id_receiver]["conversa"] = {new_message}
+
+                                response = "SND=SERVER&STATUS=OK--RESP MIS" # Mensagem inserida com sucesso
+
+                            except IndexError:
+                                response = "SND=SERVER&STATUS=OK--RESP PNNE" # parametro necessario não encontrado
+                        else:
+                            response = "SND=SERVER&STATUS=OK--RESP PNNE" # parametro necessario não encontrado
+                    case _:
+                        response = "SND=SERVER&STATUS=OK--RESP RNF" # recurso não encontrado
+            case _:
+                response = "SND=SERVER&STATUS=OK--RESP MNS" # metodo não encontrado
+
+    except Exception:
+        response = "SND=SERVER&STATUS=ERR--RESP UE" # erro inesperado
 
         
     print("Respondido :", response)
